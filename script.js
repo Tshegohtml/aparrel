6dddfae9b8eec8ab8a8d525840d0b73f01c7a2b8
Tshegohtml <ytmotlhalane@gmail.com>
2024-05-02 13:25:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const we = document.getElementById("we")
+const cs = document.getElementById("cs")
+const paragraph = document.getElementById("paragraph")
+
+
+function info(){
+    we.textContent = "We're"
+    cs.textContent = "coming soon"
+    paragraph.textContent = "Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals."
+    
+}
+info()
